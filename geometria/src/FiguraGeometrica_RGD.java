bb4011076df6b1b1294caeca6d080d48372a669a
garri <garri@10.0.2.15>
2021-03-04 02:16:37 +0100

    Version Refactorizada

--- geometria/src/FiguraGeometrica_RGD.java
+++ geometria/src/FiguraGeometrica_RGD.java
@@ -1,9 +1,9 @@
 
-public abstract class FiguraGeometrica {
+public abstract class FiguraGeometrica_RGD {
 
 	protected String tipoFigura;
 
-	public FiguraGeometrica(String tipoFigura) {
+	public FiguraGeometrica_RGD(String tipoFigura) {
 		super();
 		this.tipoFigura = tipoFigura;
 	}
--- geometria/src/Rectangulo_RGD.java
+++ geometria/src/Rectangulo_RGD.java
@@ -1,9 +1,9 @@
 
-public class Rectangulo extends FiguraGeometrica {
+public class Rectangulo_RGD extends FiguraGeometrica_RGD {
 	private double l1;
 	private double l2;
 	
-	public Rectangulo(String tipoFigura, double lG, double lP) {
+	public Rectangulo_RGD(String tipoFigura, double lG, double lP) {
 		super(tipoFigura);
 		l1 = lG;
 		l2 = lP;
